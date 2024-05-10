942d6bda5cce2748bc90a095117a943e5180bd72
Kostian Oleg <159447875+PaRaDoXfour@users.noreply.github.com>
2024-05-11 00:33:36 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace ЛР_20_Webbrowser_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.buttonForvard = new System.Windows.Forms.Button();
+            this.buttonBack = new System.Windows.Forms.Button();
+            this.buttonHome = new System.Windows.Forms.Button();
+            this.buttonSaveAs = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.comboBox = new System.Windows.Forms.ComboBox();
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonSearch.Location = new System.Drawing.Point(12, 34);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(102, 34);
+            this.buttonSearch.TabIndex = 0;
+            this.buttonSearch.Text = "Пошук";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // buttonForvard
+            // 
+            this.buttonForvard.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonForvard.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonForvard.Location = new System.Drawing.Point(201, 34);
+            this.buttonForvard.Name = "buttonForvard";
+            this.buttonForvard.Size = new System.Drawing.Size(96, 34);
+            this.buttonForvard.TabIndex = 1;
+            this.buttonForvard.Text = "Forvard";
+            this.buttonForvard.UseVisualStyleBackColor = true;
+            this.buttonForvard.Click += new System.EventHandler(this.buttonForvard_Click);
+            // 
+            // buttonBack
+            // 
+            this.buttonBack.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonBack.Location = new System.Drawing.Point(120, 34);
+            this.buttonBack.Name = "buttonBack";
+            this.buttonBack.Size = new System.Drawing.Size(75, 34);
+            this.buttonBack.TabIndex = 2;
+            this.buttonBack.Text = "Back";
+            this.buttonBack.UseVisualStyleBackColor = true;
+            this.buttonBack.Click += new System.EventHandler(this.buttonBack_Click);
+            // 
+            // buttonHome
+            // 
+            this.buttonHome.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonHome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonHome.Location = new System.Drawing.Point(303, 34);
+            this.buttonHome.Name = "buttonHome";
+            this.buttonHome.Size = new System.Drawing.Size(86, 34);
+            this.buttonHome.TabIndex = 3;
+            this.buttonHome.Text = "Home";
+            this.buttonHome.UseVisualStyleBackColor = true;
+            this.buttonHome.Click += new System.EventHandler(this.buttonHome_Click);
+            // 
+            // buttonSaveAs
+            // 
+            this.buttonSaveAs.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonSaveAs.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonSaveAs.Location = new System.Drawing.Point(395, 34);
+            this.buttonSaveAs.Name = "buttonSaveAs";
+            this.buttonSaveAs.Size = new System.Drawing.Size(124, 34);
+            this.buttonSaveAs.TabIndex = 4;
+            this.buttonSaveAs.Text = "Save as...";
+            this.buttonSaveAs.UseVisualStyleBackColor = true;
+            this.buttonSaveAs.Click += new System.EventHandler(this.buttonSaveAs_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox.Location = new System.Drawing.Point(525, 36);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(228, 30);
+            this.textBox.TabIndex = 5;
+            // 
+            // comboBox
+            // 
+            this.comboBox.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.comboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox.FormattingEnabled = true;
+            this.comboBox.Items.AddRange(new object[] {
+            "youtube.com",
+            "google.com",
+            "Gmail.com"});
+            this.comboBox.Location = new System.Drawing.Point(759, 34);
+            this.comboBox.Name = "comboBox";
+            this.comboBox.Size = new System.Drawing.Size(164, 33);
+            this.comboBox.TabIndex = 6;
+            this.comboBox.Text = "Вибране";
+            this.comboBox.SelectedIndexChanged += new System.EventHandler(this.comboBox_SelectedIndexChanged);
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser.Location = new System.Drawing.Point(27, 100);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(726, 441);
+            this.webBrowser.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkCyan;
+            this.ClientSize = new System.Drawing.Size(981, 553);
+            this.Controls.Add(this.webBrowser);
+            this.Controls.Add(this.comboBox);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.buttonSaveAs);
+            this.Controls.Add(this.buttonHome);
+            this.Controls.Add(this.buttonBack);
+            this.Controls.Add(this.buttonForvard);
+            this.Controls.Add(this.buttonSearch);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "My Browser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.Button buttonForvard;
+        private System.Windows.Forms.Button buttonBack;
+        private System.Windows.Forms.Button buttonHome;
+        private System.Windows.Forms.Button buttonSaveAs;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.ComboBox comboBox;
+        private System.Windows.Forms.WebBrowser webBrowser;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace ЛР_20_Webbrowser_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            webBrowser.Navigate(textBox.Text);
+        }
+
+        private void buttonForvard_Click(object sender, EventArgs e)
+        {
+            webBrowser.GoForward();
+        }
+
+        private void buttonBack_Click(object sender, EventArgs e)
+        {
+            webBrowser.GoBack();
+        }
+
+        private void buttonHome_Click(object sender, EventArgs e)
+        {
+            webBrowser.Navigate("youtube.com");
+        }
+
+        private void buttonSaveAs_Click(object sender, EventArgs e)
+        {
+            webBrowser.Print();
+        }
+
+        private void comboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox.SelectedIndex)
+            {
+                case 0: webBrowser.Navigate("youtube.com"); break;
+                case 1: webBrowser.Navigate("google.com"); break;
+                case 2: webBrowser.Navigate("Gmail.com"); break;
+            }
+
+        }
+    }
+}
